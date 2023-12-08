85a88327258d51048411a3e42827d93af862554d
kokotyan <kokochi.0207@gmail.com>
2023-12-09 07:05:25 +0900

    あらかたできたのでコミット

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// script.js
+$(document).ready(function () {
+    var $menu = $("#global_nav");
+    var $openBtn = $(".openbtn");
 
-document.getElementById('drawer_toggle').addEventListener('click', function () {
-    document.getElementById('global_nav').classList.toggle('show');
+    $openBtn.click(function (e) {
+        e.stopPropagation();
+        console.log("openBtn clicked");
+        $openBtn.toggleClass('active');
+        $menu.slideToggle(300);
+    });
+
+    $menu.click(function (e) {
+        e.stopPropagation();
+        console.log("menu clicked");
+    });
+
+    $(document).click(function (e) {
+        console.log("document clicked");
+        if (!$menu.is(e.target) && $menu.has(e.target).length === 0) {
+            $openBtn.removeClass('active');
+            $menu.slideUp(300);
+        }
+    });
 });
